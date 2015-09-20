a3808eb717f72a7518ef349109288935765f0733
kopfnicker92 <florvandenburg@gmail.com>
2015-09-20 20:34:36 +0200

    ..
    ..

--- papa.php
+++ papa.php
@@ -1,169 +1,169 @@
 <!DOCTYPE html>
 <html lang="en">
 
-	<head>
+<head>
 
-		<title>DWM PHP-TP</title>
-		<meta charset="UTF-8" />
-		<link rel="stylesheet" href="css/stylepapa.css">
+	<title>DWM PHP-TP</title>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="css/stylepapa.css">
 
-	</head> 
-	<body>
+</head> 
+<body>
 
-		<div class="container">
+	<div class="container">
 
-			<h1>Dad, I need your car to:</h1>
+		<h1>Dad, I need your car to:</h1>
 
-			<form action="#" method="GET">
+		<form action="#" method="GET">
 
-				<div class="form_left">
+			<div class="form_left">
 
-					<fieldset>
+				<fieldset>
 
-						<legend>test your new tires in the forest</legend>
+					<legend>test your new tires in the forest</legend>
 
-						<label for="yes">Yes</label>
-						<input type="radio" id="yes" name="tires" value="yes" required <?php if($GET['tires'] == "yes") { echo 'checked="checked"'; } ?> />
+					<label for="yes">Yes</label>
+					<input type="radio" id="yes" name="tires" value="yes" required <?php if($GET['tires'] == "yes") { echo 'checked="checked"'; } ?> />
 
-						<label for="no">No</label>
-						<input type="radio" id="no" name="tires" value="no" required <?php if($GET['tires'] == "no") { echo 'checked="checked"'; } ?> />
+					<label for="no">No</label>
+					<input type="radio" id="no" name="tires" value="no" required <?php if($GET['tires'] == "no") { echo 'checked="checked"'; } ?> />
 
-					</fieldset>
+				</fieldset>
 
-					<fieldset>
+				<fieldset>
 
-						<legend>drop Mommy at the nearest Desert</legend>
+					<legend>drop Mommy at the nearest Desert</legend>
 
-						<label for="yes">Yes</label>
-						<input type="radio" id="yes" name="mommy" value="yes" required <?php if($GET['mommy'] == "yes") { echo 'checked="checked"'; } ?> />
+					<label for="yes">Yes</label>
+					<input type="radio" id="yes" name="mommy" value="yes" required <?php if($GET['mommy'] == "yes") { echo 'checked="checked"'; } ?> />
 
-						<label for="no">No</label>
-						<input type="radio" id="no" name="mommy" value="no" required <?php if($GET['mommy'] == "no") { echo 'checked="checked"'; } ?> />
+					<label for="no">No</label>
+					<input type="radio" id="no" name="mommy" value="no" required <?php if($GET['mommy'] == "no") { echo 'checked="checked"'; } ?> />
 
-					</fieldset>
+				</fieldset>
 
-					<fieldset>
+				<fieldset>
 
-						<legend>buy some groceries for the family</legend>
+					<legend>buy some groceries for the family</legend>
 
-						<label for="yes">Yes</label>
-						<input type="radio" id="yes" name="groceries" value="yes" required <?php if($GET['groceries'] == "yes") { echo 'checked="checked"'; } ?> />
+					<label for="yes">Yes</label>
+					<input type="radio" id="yes" name="groceries" value="yes" required <?php if($GET['groceries'] == "yes") { echo 'checked="checked"'; } ?> />
 
-						<label for="no">No</label>
-						<input type="radio" id="no" name="groceries" value="no" required <?php if($GET['groceries'] == "no") { echo 'checked="checked"'; } ?> />
+					<label for="no">No</label>
+					<input type="radio" id="no" name="groceries" value="no" required <?php if($GET['groceries'] == "no") { echo 'checked="checked"'; } ?> />
 
-					</fieldset>
+				</fieldset>
 
-				</div>
+			</div>
 
-				<div class="form_right">
+			<div class="form_right">
 
-					<fieldset>
+				<fieldset>
 
-						<legend>buy a bottle of Whisky for you</legend>
+					<legend>buy a bottle of Whisky for you</legend>
 
-						<label for="yes">Yes</label>
-						<input type="radio" id="yes" name="whisky" value="yes" required <?php if($GET['whisky'] == "yes") { echo 'checked="checked"'; } ?> />
+					<label for="yes">Yes</label>
+					<input type="radio" id="yes" name="whisky" value="yes" required <?php if($GET['whisky'] == "yes") { echo 'checked="checked"'; } ?> />
 
-						<label for="no">No</label>
-						<input type="radio" id="no" name="whisky" value="no" required <?php if($GET['whisky'] == "no") { echo 'checked="checked"'; } ?> />
+					<label for="no">No</label>
+					<input type="radio" id="no" name="whisky" value="no" required <?php if($GET['whisky'] == "no") { echo 'checked="checked"'; } ?> />
 
-					</fieldset>
+				</fieldset>
 
-					<fieldset>
+				<fieldset>
 
-						<legend>visit my new girlfriend</legend>
+					<legend>visit my new girlfriend</legend>
 
-						<label for="yes">Yes</label>
-						<input type="radio" id="yes" name="girlfriend" value="yes" required <?php if($GET['girlfriend'] == "yes") { echo 'checked="checked"'; } ?> />
+					<label for="yes">Yes</label>
+					<input type="radio" id="yes" name="girlfriend" value="yes" required <?php if($GET['girlfriend'] == "yes") { echo 'checked="checked"'; } ?> />
 
-						<label for="no">No</label>
-						<input type="radio" id="no" name="girlfriend" value="no" required <?php if($GET['girlfriend'] == "no") { echo 'checked="checked"'; } ?> />
+					<label for="no">No</label>
+					<input type="radio" id="no" name="girlfriend" value="no" required <?php if($GET['girlfriend'] == "no") { echo 'checked="checked"'; } ?> />
 
-					</fieldset>
+				</fieldset>
 
-					<fieldset>
+				<fieldset>
 
-						<legend>cruise through the city</legend>
+					<legend>cruise through the city</legend>
 
-						<label for="yes">Yes</label>
-						<input type="radio" id="yes" name="city" value="yes" required <?php if($GET['city'] == "yes") { echo 'checked="checked"'; } ?> />
+					<label for="yes">Yes</label>
+					<input type="radio" id="yes" name="city" value="yes" required <?php if($GET['city'] == "yes") { echo 'checked="checked"'; } ?> />
 
-						<label for="no">No</label>
-						<input type="radio" id="no" name="city" value="no" required <?php if($GET['city'] == "no") { echo 'checked="checked"'; } ?> />
+					<label for="no">No</label>
+					<input type="radio" id="no" name="city" value="no" required <?php if($GET['city'] == "no") { echo 'checked="checked"'; } ?> />
 
-					</fieldset>
+				</fieldset>
 
-				</div>
+			</div>
 
-				<div class="clear"></div>
+			<div class="clear"></div>
 
-				<button type="submit">Are you giving me the keys now?</button>
+			<button type="submit">Are you giving me the keys now?</button>
  
 
-			</form> 
+		</form> 
 
-		</div>
-
-
-		<?php
-
-			//stocking the possible anwers in $message[".."]
-			$message["yes"] = '<div class="answer">OK, here are the keys my lovely son!<img src ="img/bmw.png" alt="bmw of dad" width="400px" /></div>';
-			$message["no"] = '<div class="answer">NO! Get the fuck away from me!</div>';
-			$message["maybe"] = '<div class="answer">Only if you drive as slow as grandma!</div>';
-
-			//declare the funtion foo
-			function foo() {
-				echo '<div class="answer">Always clicking yes is easy you Clown,  take your time!</div>';
-			}
+	</div>
 
 
-			//check if the request method is GET
-			if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-				// checking if the different options are set
-				if(isset($_GET['tires']) && isset($_GET['mommy']) && isset($_GET['groceries']) && isset($_GET['whisky']) && isset($_GET['girlfriend']) && isset($_GET['city'])){
+	<?php
 
-						//getting the different scenarios and stocking them in a variable
-						$tires = $_GET["tires"];
-						$mommy = $_GET["mommy"];
-						$groceries = $_GET["groceries"];
-						$whisky = $_GET["whisky"];
-						$girlfriend = $_GET["girlfriend"];
-						$city = $_GET["city"];
+		//stocking the possible anwers in $message[".."]
+		$message["yes"] = '<div class="answer">OK, here are the keys my lovely son!<img src ="img/bmw.png" alt="bmw of dad" width="400px" /></div>';
+		$message["no"] = '<div class="answer">NO! Get the fuck away from me!</div>';
+		$message["maybe"] = '<div class="answer">Only if you drive as slow as grandma!</div>';
 
-						//if condition
-						if ($tires == "no" && $mommy == "yes" && $groceries == "yes"
-						&& $whisky == "yes" && $girlfriend == "no" && $city == "no") {
+		//declare the funtion foo
+		function foo() {
+			echo '<div class="answer">Always clicking yes is easy you Clown, take your time!</div>';
+		}
 
-							//displaying the message
-							echo $message["yes"];
 
-					} 	else if ($tires == "no" && $mommy == "yes" && $groceries == "yes" && $whisky == "yes" && $girlfriend == "yes" && $city == "no") {
+		//check if the request method is GET
+		if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+			// checking if the different options are set
+			if(isset($_GET['tires']) && isset($_GET['mommy']) && isset($_GET['groceries']) && isset($_GET['whisky']) && isset($_GET['girlfriend']) && isset($_GET['city'])){
 
-							echo $message["maybe"];
+					//getting the different scenarios and stocking them in a variable
+					$tires = $_GET["tires"];
+					$mommy = $_GET["mommy"];
+					$groceries = $_GET["groceries"];
+					$whisky = $_GET["whisky"];
+					$girlfriend = $_GET["girlfriend"];
+					$city = $_GET["city"];
 
-					}	else if ($tires == "yes" && $mommy == "no" && $groceries == "yes" && $whisky == "yes" && $girlfriend == "yes" && $city == "no") { 
+					//if condition
+					if ($tires == "no" && $mommy == "yes" && $groceries == "yes"
+					&& $whisky == "yes" && $girlfriend == "no" && $city == "no") {
 
-							echo $message["maybe"];
+						//displaying the message
+						echo $message["yes"];
 
-					} else if ($tires == "yes" && $mommy == "yes" && $groceries == "yes" && $whisky == "yes" && $girlfriend == "yes" && $city == "yes"){
+				} 	else if ($tires == "no" && $mommy == "yes" && $groceries == "yes" && $whisky == "yes" && $girlfriend == "yes" && $city == "no") {
 
-							//call the function foo
-							echo foo();
+						echo $message["maybe"];
 
-					} else {
+				}	else if ($tires == "yes" && $mommy == "no" && $groceries == "yes" && $whisky == "yes" && $girlfriend == "yes" && $city == "no") { 
 
-							echo $message["no"];
+						echo $message["maybe"];
 
-					}
+				} else if ($tires == "yes" && $mommy == "yes" && $groceries == "yes" && $whisky == "yes" && $girlfriend == "yes" && $city == "yes"){
+
+						//call the function foo
+						echo foo();
+
+				} else {
+
+						echo $message["no"];
 
 				}
-				
-    		}
-			
-		?>
 
-	</body>	
+			}
+				
+    	}
+			
+	?>
+
+</body>	
 </html>
 
