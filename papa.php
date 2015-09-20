3abc201071b9a82cd2b65d4bc72efbf5ea7fc6ea
kopfnicker92 <florvandenburg@gmail.com>
2015-09-20 21:12:57 +0200

    Update papa.php

--- papa.php
+++ papa.php
@@ -18,7 +18,7 @@
 
 				<fieldset>
 
-					<legend>test your new tires in the Forest</legend>
+					<legend>test your new tires in the forest</legend>
 
 					<label for="yes">Yes</label>
 					<input type="radio" id="yes" name="tires" value="yes" required <?php if($GET['tires'] == "yes") { echo 'checked="checked"'; } ?> />
@@ -30,7 +30,7 @@
 
 				<fieldset>
 
-					<legend>drop Mommy at the nearest Desert</legend>
+					<legend>drop Mommy at the nearest desert</legend>
 
 					<label for="yes">Yes</label>
 					<input type="radio" id="yes" name="mommy" value="yes" required <?php if($GET['mommy'] == "yes") { echo 'checked="checked"'; } ?> />
@@ -42,7 +42,7 @@
 
 				<fieldset>
 
-					<legend>buy some groceries for the Family</legend>
+					<legend>buy some groceries for the family</legend>
 
 					<label for="yes">Yes</label>
 					<input type="radio" id="yes" name="groceries" value="yes" required <?php if($GET['groceries'] == "yes") { echo 'checked="checked"'; } ?> />
@@ -58,7 +58,7 @@
 
 				<fieldset>
 
-					<legend>buy a Bottle of Whisky for you</legend>
+					<legend>buy a bottle of whisky for you</legend>
 
 					<label for="yes">Yes</label>
 					<input type="radio" id="yes" name="whisky" value="yes" required <?php if($GET['whisky'] == "yes") { echo 'checked="checked"'; } ?> />
@@ -70,7 +70,7 @@
 
 				<fieldset>
 
-					<legend>visit my new Girlfriend</legend>
+					<legend>visit my new girlfriend</legend>
 
 					<label for="yes">Yes</label>
 					<input type="radio" id="yes" name="girlfriend" value="yes" required <?php if($GET['girlfriend'] == "yes") { echo 'checked="checked"'; } ?> />
@@ -82,7 +82,7 @@
 
 				<fieldset>
 
-					<legend>cruise through the City</legend>
+					<legend>cruise through the city</legend>
 
 					<label for="yes">Yes</label>
 					<input type="radio" id="yes" name="city" value="yes" required <?php if($GET['city'] == "yes") { echo 'checked="checked"'; } ?> />
